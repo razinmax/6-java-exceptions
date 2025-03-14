7262b40bee9a8350b7fa2dd7585dec87333ae9f6
Max Razin <razinmax02@gmail.com>
2025-03-14 13:38:50 +0500

    task06 complete

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -4,13 +4,20 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         new Task06Main().printMethodName();
-         */
+
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        // Получаем стек вызовов
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
 
+        // stackTrace[0] — это метод printMethodName, stackTrace[1] — это метод main,
+        // stackTrace[2] — это метод, который вызвал main (если это было в другом контексте).
+        String methodName = stackTrace[2].getMethodName(); // Получаем имя метода, который вызвал printMethodName
+
+        // Выводим имя метода, который вызвал printMethodName
+        System.out.print(methodName);
+    }
 }
